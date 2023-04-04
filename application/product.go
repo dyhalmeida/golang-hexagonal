61eb4e4975c3e7eb31371e97ec56191246a5c577
dyhalmeida <dyhalmeida@gmail.com>
2023-04-04 18:15:32 -0300

    feat: add product constructor

--- application/product.go
+++ application/product.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 
 	"errors"
 )
@@ -30,6 +31,14 @@ type Product struct {
 	Name string `valid:"required"`
 	Price float64 `valid:"float,optional"`
 	Status string `valid:"required"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		ID: uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product
 }
 
 func (p *Product) IsValid() (bool, error) {
