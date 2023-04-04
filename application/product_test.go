367c10ed4d4cfc4c9b9f6f3c8d53aa7b91b80f92
dyhalmeida <dyhalmeida@gmail.com>
2023-04-04 18:09:21 -0300

    test: add test to IsValid product

--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"github/com/dyhalmeida/golang-hexagonal/application"
 	"testing"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,4 +34,28 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "The price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Any Name"
+	product.Status = application.DISABLED
+	product.Price = 0
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater or equal zero", err.Error())
+
 }
