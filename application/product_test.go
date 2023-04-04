5f8fe392bbb9bad8f70ad9499feca1cb5cff48b6
dyhalmeida <dyhalmeida@gmail.com>
2023-04-04 17:32:14 -0300

    test: add test to enable product

---
+++ application/product_test.go
@@ -0,0 +1,22 @@
+package application_test
+
+import (
+	"github/com/dyhalmeida/golang-hexagonal/application"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Any Name"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+}
