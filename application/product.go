9c6725fa39fd627aa4099b9dd8ccac7de93be136
dyhalmeida <dyhalmeida@gmail.com>
2023-04-04 17:47:29 -0300

    feat: add disable product

--- application/product.go
+++ application/product.go
@@ -23,6 +23,11 @@ type Product struct {
 	Price float64
 	Status string
 }
+
+// func (p *Product) IsValid() (bool, error) {
+
+// }
+
 func (p *Product) Enable() error {
 	if p.Price > 0 {
 		p.Status = ENABLED
@@ -31,6 +36,13 @@ func (p *Product) Enable() error {
 	return errors.New("The price must be greater than zero to enable the product")
 }
 
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("The price must be zero in order to have the product disabled")
+}
 
 func (p *Product) GetID() string {
 	return p.ID
